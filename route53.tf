9eb9f4c4f58ccdaef4858bf92a05cd91b3ac5d8c
riqardos <kastiak.richard@gmail.com>
2024-01-29 09:59:33 +0100

    fix route53

--- route53.tf
+++ route53.tf
@@ -27,7 +27,7 @@ resource "aws_acm_certificate" "alb" {
 
 resource "aws_route53_record" "cert_alb" {
   for_each = {
-    for dvo in aws_acm_certificate.alb[0].domain_validation_options : dvo.domain_name => {
+    for dvo in try(aws_acm_certificate.alb[0].domain_validation_options, []) : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
