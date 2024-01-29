e4b7aede49c6e18f6cd2567490e8c2bb2f06f58e
riqardos <kastiak.richard@gmail.com>
2024-01-29 13:18:00 +0100

    state backend example

--- examples/development/providers.tf
+++ examples/development/providers.tf
@@ -1,3 +1,15 @@
+# Good practice is to use s3 bucket to save the state and dynamodb table as lock to restrict access to the state
+# For example:
+#
+# terraform {
+#   backend "s3" {
+#     bucket = "appmixer-terraform-state"
+#     key    = "appmixer/terraform.tfstate"
+#     region = "eu-central-1"
+#   }
+# }
+
+
 provider "aws" {
   region = "eu-central-1"
 }
