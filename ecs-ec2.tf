4fbbcb22f7dcf7d395cb2077f7eb58bab9e61432
Andrej Golis <andrej.golis@gmail.com>
2024-05-16 10:31:05 +0400

    #6 Move comment outside of expression

--- ecs-ec2.tf
+++ ecs-ec2.tf
@@ -49,9 +49,10 @@ locals {
         "ECS_ENABLE_HIGH_DENSITY_ENI=true",
         "ECS_ENABLE_SPOT_INSTANCE_DRAINING=true",
         "ECS_ENGINE_AUTH_TYPE=dockercfg",
-        "ECS_ENGINE_AUTH_DATA=${sensitive(try(base64decode(var.ecs_registry_auth_data), \"\"))}", # pragma: allowlist secret
+        "ECS_ENGINE_AUTH_DATA=${sensitive(try(base64decode(var.ecs_registry_auth_data), \"\"))}",
         "EOF"
   ])
+ # pragma: allowlist secret
 }
 
 module "alb" {
